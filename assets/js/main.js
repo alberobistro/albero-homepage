264feac85e0d8f7fc98c157a862571e8ae753bb7
Hani Bhat <hanighulam@gmail.com>
2023-03-19 19:31:11 +0100

    fixes styles and adds Albero

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,7 @@ const translations = {
     gallery: 'Galerie',
     menu: 'Speisekarte',
     contact: 'Kontakt',
-    welcome: 'Willkommen in ',
+    welcome: 'Willkommen in <span>Albero</span',
     'welcome-subtitle':
       'Speisen aus nachhaltigen Lebensmitteln, hergestellt von regionalen Erzeugern!',
     'about-text': `Speisen aus nachhaltigen Lebensmitteln, hergestellt von regionalen Erzeugern. Darauf legen wir besonderen Wert. Unsere Tomatensauce Gustarosso wird 100% aus Tomaten von traditionellen Erzeugern einer landwirtschaftlichen Genossenschaft aus der Region Kampanien (Italien) hergestellt. Für unsere Pizzen verwenden wir feinste Fior di  Latte, ebenfalls von Erzeugern aus dieser Region. Unser hochwertiger Büffelmozzarella und Burrata wird aus der Milch der Wasserbüffel hergestellt, die auf einem Bauernhof in Brandenburg mit freiem Auslauf auf grünem Weideland leben.`,
@@ -17,10 +17,10 @@ const translations = {
     location: 'Standort',
     opening: 'Öffnungszeiten',
     'opening-times': `
-      Montag-Freitag:
-      12:00 - 22:00
-      Samstag-Sonntag:
-      16:00 - 22:00
+      Montag-Freitag:</br>
+      12:00 - 22:00</br>
+      Samstag-Sonntag:</br>
+      16:00 - 22:00</br>
     `,
     'opening-times-short': 'Mo-Fr: 12 - 22, Sam-Sonn: 16 - 22',
     phone: 'Telefon',
@@ -31,8 +31,8 @@ const translations = {
     gallery: 'Gallery',
     menu: 'Menu',
     contact: 'Contact',
-    welcome: 'Welcome in ',
-    'welcome-subtitle': 'Dishes made from sustainable food, made by local producers!',
+    welcome: 'Welcome to <span>Albero</span',
+    'welcome-subtitle': 'Dishes made from sustainable food, by local producers!',
     'about-text': `Dishes made from sustainable food, made by local producers. On it we attach particular importance. Our Gustarosso tomato sauce is made from 100% tomatoes traditional producers of an agricultural cooperative from the region Made in Campania (Italy). We use the finest Fior di for our pizzas Latte, also from producers in this region. Our high quality Buffalo mozzarella and burrata is made from the milk of the water buffalo that live on a farm in Brandenburg with free exercise on green pastureland.`,
     all: 'All',
     starters: 'Starters',
@@ -42,10 +42,10 @@ const translations = {
     location: 'Location',
     opening: 'Opening times',
     'opening-times': `
-      Monday-Friday:
-      12:00 - 22:00
-      Saturday-Sunday:
-      16:00 - 22:00
+      Monday-Friday:</br>
+      12:00 - 22:00<br/>
+      Saturday-Sunday:</br>
+      16:00 - 22:00</br>
     `,
     'opening-times-short': 'Mo-Fr: 12 - 22, Sat-Sun: 16 - 22',
     phone: 'Phone',
@@ -58,7 +58,7 @@ const translations = {
 const translateElement = (locale) => (element) => {
   const key = element.getAttribute('data-i18n')
   const translation = translations[locale][key]
-  element.innerText = translation
+  element.innerHTML = translation
 }
 
 /**
