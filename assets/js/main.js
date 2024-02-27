e91f22b49058fa1f6e0a95398a20cf216dbe1c66
Hani Bhat <hanighulam@gmail.com>
2024-02-27 16:31:27 +0100

    adds impressum and privacy policy and cookie consent

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,11 @@ const translations = {
     gallery: 'Galerie',
     menu: 'Speisekarte',
     contact: 'Kontakt',
+    impressum: 'Impressum',
+    privacy: 'Datenschutz',
+    'cookie-consent':
+      'Diese Website verwendet Cookies, um sicherzustellen, dass Sie das beste Erlebnis auf unserer Website erhalten.',
+    'cookie-consent-btn': 'Verstanden!',
     welcome: 'Willkommen in <span>Albero</span',
     'welcome-subtitle':
       'Speisen aus nachhaltigen Lebensmitteln, hergestellt von regionalen Erzeugern!',
@@ -24,6 +29,58 @@ const translations = {
     `,
     'opening-times-short': 'Mo-Fr: 12 - 22, Sam-Sonn: 16 - 22',
     phone: 'Telefon',
+    'impressum-content': `
+    <h4>Angaben Gemäss § 5 TMG</h4>
+      <div class="impressum-section">
+        <p>Bistro Albero</p>
+        <p>Lehrter Str. 24b</p>
+        <p>10557 Berlin</p>
+      </div>
+      <div class="impressum-section">
+        <p>Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz</p>
+        <p>DE348004932</p>
+      </div>
+      <div class="impressum-section">
+        <p>
+          <span>Telefon:</span>
+          <a href="tel:+4915210256215">+49 152 10256215</a><br />
+          <span>Email:</span>
+          <a href="mailto:alberobistro@gmail.com">alberobistro@gmail.com</a><br />
+        </p>
+      </div>
+      <div class="impressum-section">
+        <p>Vertreten Durch</p>
+        <p>Mehmed Mustafa</p>
+      </div>
+      `,
+    'privacy-content': `<p>Stand: 27. Februar 2024</p>
+    <p>Bistro Albero ("uns", "wir" oder "unser") betreibt die Website https://albero-berlin.de/ (der "Dienst").</p>
+    <p>Diese Seite informiert Sie über unsere Richtlinien zur Erhebung, Nutzung und Offenlegung von personenbezogenen Daten, wenn Sie unseren Dienst nutzen, sowie über die Ihnen zustehenden Wahlmöglichkeiten in Bezug auf diese Daten.</p>
+    <p>Wir erfassen über unsere Website keine personenbezogenen Daten.</p>
+    <h2>Dienstleister</h2>
+    <p>Wir können aus folgenden Gründen auf Drittanbieter und Einzelpersonen zurückgreifen:</p>
+    <ul>
+        <li>Um unseren Dienst zu erleichtern;</li>
+        <li>Um den Dienst in unserem Auftrag bereitzustellen;</li>
+        <li>Um Dienstleistungen im Zusammenhang mit unserem Dienst durchzuführen; oder</li>
+        <li>Um uns bei der Analyse der Nutzung unseres Dienstes zu unterstützen.</li>
+    </ul>
+    <p>Bitte beachten Sie jedoch, dass diese Drittanbieter nur Zugriff auf Ihre personenbezogenen Daten haben, um diese Aufgaben in unserem Auftrag auszuführen, und nicht berechtigt sind, sie für andere Zwecke offenzulegen oder zu verwenden.</p>
+    <h2>Analysen</h2>
+    <p>Wir können Drittanbieter-Dienstleister wie Google Analytics und Firebase verwenden, um die Nutzung unseres Dienstes zu überwachen und zu analysieren.</p>
+    <p>Diese Drittanbieter-Dienstleister haben ihre eigenen Datenschutzrichtlinien, die regeln, wie sie solche Informationen verwenden.</p>
+    <h2>Links zu anderen Websites</h2>
+    <p>Unser Dienst kann Links zu anderen Websites enthalten, die nicht von uns betrieben werden. Wenn Sie auf einen Link eines Drittanbieters klicken, werden Sie direkt auf die Website dieses Drittanbieters weitergeleitet. Wir empfehlen Ihnen dringend, die Datenschutzrichtlinie jeder von Ihnen besuchten Website zu überprüfen.</p>
+    <p>Wir haben keine Kontrolle über Inhalte, Datenschutzrichtlinien oder Praktiken von Websites oder Diensten Dritter und übernehmen keine Verantwortung dafür.</p>
+    <h2>Änderungen an dieser Datenschutzrichtlinie</h2>
+    <p>Wir können unsere Datenschutzrichtlinie von Zeit zu Zeit aktualisieren. Wir werden Sie über etwaige Änderungen informieren, indem wir die neue Datenschutzrichtlinie auf dieser Seite veröffentlichen.</p>
+    <p>Es wird empfohlen, diese Datenschutzrichtlinie regelmäßig auf Änderungen zu überprüfen. Änderungen dieser Datenschutzrichtlinie sind wirksam, wenn sie auf dieser Seite veröffentlicht werden.</p>
+    <h2>Kontakt</h2>
+    <p>Wenn Sie Fragen zu dieser Datenschutzrichtlinie haben, kontaktieren Sie uns bitte:</p>
+    <ul>
+        <li>Per E-Mail: <a href="mailto:alberobistro@gmail.com">alberobistro@gmail.com</a></li>
+        <li>Über diese Seite auf unserer Website: https://albero-berlin.de/#contact</li>
+    </ul>`,
   },
   EN: {
     home: 'Home',
@@ -31,6 +88,11 @@ const translations = {
     gallery: 'Gallery',
     menu: 'Menu',
     contact: 'Contact',
+    impressum: 'Impressum',
+    privacy: 'Privacy Policy',
+    'cookie-consent':
+      'This website uses cookies to ensure you get the best experience on our website.',
+    'cookie-consent-btn': 'Got it!',
     welcome: 'Welcome to <span>Albero</span',
     'welcome-subtitle': 'Dishes made from sustainable food, by local producers!',
     'about-text': `Dishes made from sustainable food, made by local producers. On it we attach particular importance. Our Gustarosso tomato sauce is made from 100% tomatoes traditional producers of an agricultural cooperative from the region Made in Campania (Italy). We use the finest Fior di for our pizzas Latte, also from producers in this region. Our high quality Buffalo mozzarella and burrata is made from the milk of the water buffalo that live on a farm in Brandenburg with free exercise on green pastureland.`,
@@ -49,6 +111,59 @@ const translations = {
     `,
     'opening-times-short': 'Mo-Fr: 12 - 22, Sat-Sun: 16 - 22',
     phone: 'Phone',
+    'impressum-content': `
+    <h4>Angaben Gemäss § 5 TMG</h4>
+      <div class="impressum-section">
+        <p>Bistro Albero</p>
+        <p>Lehrter Str. 24b</p>
+        <p>10557 Berlin</p>
+      </div>
+      <div class="impressum-section">
+        <p>Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz</p>
+        <p>DE348004932</p>
+      </div>
+      <div class="impressum-section">
+        <p>
+          <span>Phone:</span>
+          <a href="tel:+4915210256215">+49 152 10256215</a><br />
+          <span>Email:</span>
+          <a href="mailto:alberobistro@gmail.com">alberobistro@gmail.com</a><br />
+        </p>
+      </div>
+      <div class="impressum-section">
+        <p>Represented by</p>
+        <p>Mehmed Mustafa</p>
+      </div>
+      `,
+    'privacy-content': `
+    <p>Effective date: February 27, 2024</p>
+    <p>Bistro Albero ("us", "we", or "our") operates the website https://albero-berlin.de/ (the "Service").</p>
+    <p>This page informs you of our policies regarding the collection, use, and disclosure of personal data when you use our Service and the choices you have associated with that data.</p>
+    <p>We do not collect any personally identifiable information or personal data through our website.</p>
+    <h2>Service Providers</h2>
+    <p>We may employ third-party companies and individuals due to the following reasons:</p>
+    <ul>
+        <li>To facilitate our Service;</li>
+        <li>To provide the Service on our behalf;</li>
+        <li>To perform Service-related services; or</li>
+        <li>To assist us in analyzing how our Service is used.</li>
+    </ul>
+    <p>However, please note that these third parties have access to your Personal Data only to perform these tasks on our behalf and are obligated not to disclose or use it for any other purpose.</p>
+    <h2>Analytics</h2>
+    <p>We may use third-party Service Providers, such as Google Analytics and Firebase, to monitor and analyze the use of our Service.</p>
+    <p>These third-party service providers have their own privacy policies addressing how they use such information.</p>
+    <h2>Links to Other Sites</h2>
+    <p>Our Service may contain links to other sites that are not operated by us. If you click a third-party link, you will be directed to that third party's site. We strongly advise you to review the Privacy Policy of every site you visit.</p>
+    <p>We have no control over and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
+    <h2>Changes to This Privacy Policy</h2>
+    <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.</p>
+    <p>You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
+    <h2>Contact Us</h2>
+    <p>If you have any questions about this Privacy Policy, please contact us:</p>
+    <ul>
+        <li>By email: <a href="mailto:alberobistro@gmail.com">alberobistro@gmail.com</a></li>
+        <li>By visiting this page on our website: https://albero-berlin.de/#contact</li>
+    </ul>`,
   },
 }
 
@@ -73,12 +188,21 @@ const translateElement = (locale) => (element) => {
   const languageBtn = document.getElementById('language-btn')
   const currentLanguage = localStorage.getItem('language') || 'DE'
   const availableLanguage = currentLanguage === 'DE' ? 'EN' : 'DE'
-  languageBtn.innerHTML = availableLanguage
+  if (languageBtn) {
+    languageBtn.innerHTML = availableLanguage
+    languageBtn.onclick = (e) => {
+      const selectedLanguage = e.target.innerHTML
+      localStorage.setItem('language', selectedLanguage)
+      location.reload()
+    }
+  }
 
-  languageBtn.onclick = (e) => {
-    const selectedLanguage = e.target.innerHTML
-    localStorage.setItem('language', selectedLanguage)
-    location.reload()
+  const cookieConsent = document.getElementById('cookieConsent')
+  const cookieConsentBtn = document.querySelector('#cookieConsent > button')
+  cookieConsentBtn.onclick = () => {
+    // Set a cookie to remember user's consent
+    localStorage.setItem('cookieConsentAccepted', 'true')
+    cookieConsent.style.display = 'none'
   }
 
   // When the page content is ready...
@@ -87,6 +211,11 @@ const translateElement = (locale) => (element) => {
       // Find all elements that have the key attribute
       .querySelectorAll('[data-i18n]')
       .forEach(translateElement(currentLanguage))
+
+    // Check if the user has already accepted cookies
+    if (localStorage.getItem('cookieConsentAccepted')) {
+      cookieConsent.style.display = 'none'
+    }
   })
 
   /**
